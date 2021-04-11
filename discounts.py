988ac0530d4d2abd109708e10fc703c9ad57bd38
Javier Rodriguez <fjrpascual@gmail.com>
2021-04-11 19:06:26 +0200

    added reduced price if n or more items pricing rule

--- discounts.py
+++ discounts.py
@@ -5,3 +5,10 @@ class TwoForOneItem(PricingRule):
     def __init__(self, item: Item):
         super().__init__(f"TwoForOne{item.code}",
                          lambda item_list: (item_list.count(item.code) // 2) * -item.unit_price)
+
+
+class ReducedPriceIfNOrMoreItems(PricingRule):
+    def __init__(self, item: Item, min_items: int, new_price: float):
+        super().__init__(f"ReducedPriceIf{min_items}OrMore{item.code}",
+                         lambda item_list: 0 if (total := min_items.count(item.code)) < min_items
+                         else (new_price - item.unit_price) * total)
