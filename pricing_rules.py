275466f0f30077c610d1f9d451b669c19318fecd
Javier Rodriguez <fjrpascual@gmail.com>
2021-04-11 18:38:20 +0200

    i believe this is the right way of calling the parent's constructor in py

--- pricing_rules.py
+++ pricing_rules.py
@@ -14,4 +14,4 @@ class Item(PricingRule):
     def __init__(self, code: str, name: str, unit_price: float):
         self.name = name
         self.unit_price = unit_price
-        super(code, lambda items: items.count(self.name) * self.unit_price)
+        super().__init__(code, lambda items: items.count(self.name) * self.unit_price)
