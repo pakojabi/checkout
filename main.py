7007e93c91c7539018b65eb9ef84a508159abb25
Javier Rodriguez <fjrpascual@gmail.com>
2021-04-11 19:57:52 +0200

    Added some dummy main code to test and fixed buggie (will add proper unit tests once i find out how to write them in python)

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
+import checkout
+import discounts
+import pricing_rules
 
 if __name__ == '__main__':
-    print('Hello')
+    voucher = pricing_rules.Item('VOUCHER', 'Gift Card', 5.0)
+    my_rules = {voucher}
+    da_checkout = checkout.Checkout(my_rules)
+    da_checkout.add_item('VOUCHER')
+    total = da_checkout.get_total()
+    print(f"total: {total}")
--- pricing_rules.py
+++ pricing_rules.py
@@ -1,12 +1,18 @@
+from collections.abc import Callable
+
+
 class PricingRule:
-    def __init__(self, code: str, calculate_price):
+    def __init__(self, code: str, calculate_price: Callable[[list[str]], float]):
         self.code = code
         self.calculate_price = calculate_price
 
     def __eq__(self, other):
         return self.code == other.code
 
-    def get_price(self, item_list):
+    def __hash__(self):
+        return hash(self.code)
+
+    def get_price(self, item_list) -> float:
         return self.calculate_price(item_list)
 
 
@@ -14,4 +20,4 @@ class Item(PricingRule):
     def __init__(self, code: str, name: str, unit_price: float):
         self.name = name
         self.unit_price = unit_price
-        super().__init__(code, lambda items: items.count(self.name) * self.unit_price)
+        super().__init__(code, lambda items: items.count(self.code) * self.unit_price)
