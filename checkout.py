f1f22cec99118f631f78a3797ba2750f77b2dba6
Javier Rodriguez <fjrpascual@gmail.com>
2021-04-11 19:55:48 +0200

    cant apply reduce directly, need to map first
    btw im just trying to do something like this (C# LINQ)
    
    return PricingRules
         .Select(p => GetPrice(p))
         .Aggregate(Add)

--- checkout.py
+++ checkout.py
@@ -1,14 +1,17 @@
 from functools import reduce
+from typing import AbstractSet
+
+from pricing_rules import PricingRule
 
 
 class Checkout:
     items = []
 
-    def __init__(self, pricing_rules):
+    def __init__(self, pricing_rules: AbstractSet[PricingRule]):
         self.pricing_rules = pricing_rules
 
     def add_item(self, item_code):
         self.items.append(item_code)
 
     def get_total(self) -> float:
-        return reduce(lambda acc, rule: acc + rule.get_price(self.items), self.pricing_rules)
+        return reduce(sum, list(map(lambda rule: rule.get_price(self.items), self.pricing_rules)))
